c7258fc63c5dd8f5f472c59922aedac23ccce978
Dave Malvin <dm.enterprienur@gmail.com>
2025-02-14 16:54:43 +0700

    Update: - Fix error of container bind9-alpine with no priviledge - Add auto-create directory for script self-sign

--- Config/Nginx/self-sign.sh
+++ Config/Nginx/self-sign.sh
@@ -3,5 +3,9 @@
 commonname=$1
 path="Config/Nginx/certs"
 
+if [[ ! -d $path ]]; then
+    mkdir -p $path
+fi
+
 openssl req -x509 -newkey rsa:2048 -keyout $path/$commonname-key.pem -out $path/$commonname-crt.pem -days 3650 -nodes -subj \
 "/C=ID/ST=Jawa Timur/L=Surabaya/O=HomeLAB/OU=Org/CN=$commonname"
